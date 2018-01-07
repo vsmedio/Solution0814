2c69862e41282e0ffb7f6d8e9bf517ea7960d824
Arvis Abromovics <info@vsmedio.lv>
2018-01-08 00:38:04 +0100

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,43 @@
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+/*
+Больше 10? Вы нам не подходите
+Создать множество чисел(Set<Integer>), занести туда 20 различных чисел.
+Удалить из множества все числа больше 10.
+
+
+Требования:
+1. Программа не должна выводить текст на экран.
+2. Программа не должна считывать значения с клавиатуры.
+3. Класс Solution должен содержать только три метода.
+4. Метод createSet() должен создавать и возвращать множество HashSet состоящих из 20 различных чисел.
+5. Метод removeAllNumbersMoreThan10() должен удалять из множества все числа больше 10.
+*/
+
+public class Solution {
+    public static HashSet<Integer> createSet() {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < 20; i++) {
+            set.add(i);
+        }
+
+        return set;
+    }
+
+    public static HashSet<Integer> removeAllNumbersMoreThan10(HashSet<Integer> set) {
+        Iterator<Integer> iterator = set.iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next() > 10) {
+                iterator.remove();
+            }
+        }
+        return set;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
